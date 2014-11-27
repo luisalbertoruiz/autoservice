9e301dd06f4360926ef14f93944cb8bbdea079b0
cgarj <carlangotas92@gmail.com>
2014-11-27 13:48:21 -0600

    se agrega tabla a index

--- app/views/cliente/edit.blade.php
+++ app/views/cliente/edit.blade.php
@@ -1,1 +1,53 @@
-/var/www/autoservice/app/views/clientes/edit.blade.php
+@extends('layout.main')
+@section('title')
+Autoservice
+@stop
+@section('header')
+	@include('layout.header')
+@stop
+@section('navbar')
+	
+@stop
+@section('content')
+<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 col-xs-offset-1">
+	<div class="panel panel-primary">
+		  <div class="panel-heading">
+				<h3 class="panel-title">Nuevo Cliente</h3>
+		  </div>
+		  <div class="panel-body">
+		  <form action="" method="POST" role="form">
+		  	<div class="form-group">
+		  	<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 col-xs-offset-1">
+		  		<label for="nombre">Nombre</label>
+		  		<input type="text" class="form-control" id="nombre"  name="name"><br>
+		  		<label for="apellido">Apellido</label>
+		  		<input type="text" class="form-control" id="apellido" name="name"><br>
+		  		<label for="calle">Calle</label>
+		  		<input type="text" class="form-control" id="calle" name="name"><br>
+		  		<label for="numero">Numero</label>
+		  		<input type="text" class="form-control" id="numero" name="numero"><br>
+		  		<label for="colonia">Colonia</label>
+		  		<input type="text" class="form-control" id="colonia" name="colonia"><br>
+		  		</div>
+		  		<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+		  		<label for="localidad">Localidad</label>
+		  		<input type="text" class="form-control" id="localidad" name="localidad"><br>
+		  		<label for="cp">Codigo Postal</label>
+		  		<input type="text" class="form-control" id="cp" name="cp"><br>
+		  		<label for="telefono">Telefonor</label>
+		  		<input type="text" class="form-control" id="telefono" name="telefono"><br>
+		  		<label for="celular">Celular</label>
+		  		<input type="text" class="form-control" id="celular" name="celular"><br>
+		  		<label for="email">Email</label>
+		  		<input type="text" class="form-control" id="email" name="email"><br>
+		  		</div>
+		  	</div>
+		  	<button type="submit" class="btn btn-primary">Submit</button>
+		  </form>
+		  </div>
+	</div>	
+</div>
+@stop
+@section('footer')
+	@include('layout.footer')
+@stop
--- app/views/cliente/index.blade.php
+++ app/views/cliente/index.blade.php
@@ -1,1 +1,54 @@
-/var/www/autoservice/app/views/clientes/index.blade.php
+@extends('layout.main')
+@section('title')
+Autoservice
+@stop
+@section('header')
+	@include('layout.header')
+@stop
+@section('navbar')
+	
+@stop
+@section('content')
+<div  clase = " col-xs-12 col-sm-12 col-md-10 col-lg-8 offset-col-md-1-col-lg offset-2 " >
+	<div  clase = " panel principal del panel " >
+		<div  clase = " Panel-encabezamiento " >
+			<h2  clase = " Panel-título " > <lapso  clase = " glyphicon glyphicon-usuario " > clientees </h2 >
+		</div >
+		<div  clase = " Panel-cuerpo " >
+		{{HTML :: enlace (URL :: a ('/ admin / cliente / Crear'), 'Nuevo cliente', array ('class' => 'btn btn-primaria btn-sm pull-derecha'))}}
+			<div  clase = " table-sensible " >
+				<tabla de  clase = " mesa de ping-hover " >
+					<thead >
+						<tr >
+							<th > Nombre (s) </th >
+							<th > Apellido (s) </th >
+							<th > sobrenombre </th >
+							<th > Playera </th >
+							<th > </th >
+							<th > </th >
+							<th > </th >
+						</tr >
+					</thead >
+					<tbody >
+						foreach ($ clientes como $ cliente)
+						<tr >
+							<td > {{$ cliente-> nombre}} </td >
+							<td > {{$ cliente-> Apellido}} </td >
+							<td > {{$ cliente-> sobrenombre}} </td >
+							<td > {{$ cliente-> playera}} </td >
+							<td > {{HTML :: enlace (URL :: a ('/ admin / cliente / Mostrar /'.$ cliente-> id),' Ver ', array (' class '=>' btn btn-éxito btn- xs '))}} </td >
+							<td > {{HTML :: enlace (URL :: a ('/ admin / cliente / editar /'.$ cliente-> id),' Editar ', array (' class '=>' btn btn btn- de alerta xs '))}} </td >
+							<td > {{HTML :: enlace (URL :: a ('/ admin / cliente / ELIMINAR /'.$ cliente-> id),' ELIMINAR ', array (' class '=>' btn btn-peligro btn- xs '))}} </td >
+						</tr >
+					endforeach
+					</tbody >
+				</mesa >
+				{{$ clientees-> Enlaces ()}}
+			</div >
+		</div >
+	</div >
+</div >
+@stop
+@section('footer')
+	@include('layout.footer')
+@stop
